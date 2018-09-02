a58cf5a2123c7f0683df4e164cb5f7d5d144c1f6
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-09-02 23:19:31 +0900

    implment logger package

---
+++ api/infrastructure/logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var writer zapcore.WriteSyncer = os.Stdout
+
+// Error write error log.
+func Error(msg string) {
+	logger := newLogger(writer)
+	defer logger.Sync()
+	logger.Error(msg)
+}
+
+func newLogger(writer zapcore.WriteSyncer) *zap.Logger {
+	atom := zap.NewAtomicLevel()
+
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = ""
+
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderCfg),
+		zapcore.Lock(writer),
+		atom,
+	))
+	return logger
+}
--- api/interfaces/handlers/entry_handler.go
+++ api/interfaces/handlers/entry_handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Khigashiguchi/khigashiguchi.com/api/interfaces/presenter"
 	"github.com/Khigashiguchi/khigashiguchi.com/api/usecase"
+	"github.com/Khigashiguchi/khigashiguchi.com/api/infrastructure/logger"
 )
 
 // Handler is interface of handling request.
